057b1d78fad2697295d43b82d98e9cd4c37aaf06
Tim <tim555@t-online.de>
2021-08-21 01:35:42 +0200

    Added First Glitch Code and Animations

--- js/indexGlitchEffect.js
+++ js/indexGlitchEffect.js
@@ -1,16 +1,29 @@
-//Glitch Elements
-var mainSectionElements;
+function disableGlitchEffect(){
+   //Object Array
+   var glitchObjectArray = getGlitchObjects();
 
-function initGlitch(elementCount){
-     //Reset glitch elemets
-     mainSectionElements = [];
-     //Create mainSectionElements and append then
-     for(var i = 0; i < elementCount; i++){
-       mainSectionElements[i] = document.createElement("div");
-       //Set class Name
-       mainSectionElements[i].className = "MainSectionGlitch";
-       //Append to login window
-       mainScreenWindow.append(mainSectionElements[i]);
-     }
+   //Disable all Objects
+   for(var i = 0; i < glitchObjectArray.length; i++){
+     glitchObjectArray[i].className += " disableGlitch";
+   }
+}
 
+function enableGlitchEffect(){
+  //Object Array
+  var glitchObjectArray = getGlitchObjects();
+
+  for(var i = 0; i < glitchObjectArray.length; i++){
+    var currentElementClassName = glitchObjectArray[i].className;
+
+    glitchObjectArray[i].className = currentElementClassName.replace("disableGlitch", "");
+  }
+}
+
+
+function getGlitchObjects(){
+  var glitchObjectArray = [];
+
+  glitchObjectArray.push(document.getElementById("gameLogin")); //Main Login
+
+  return glitchObjectArray
 }
