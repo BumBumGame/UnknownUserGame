d2086a1751889316e20f4c0f8189c86ff8da8473
Tim <tim555@t-online.de>
2021-08-23 00:34:15 +0200

    Added Glitch Animation to Cancel-Button - Also added support for interval glitch animations

--- js/indexGlitchEffect.js
+++ js/indexGlitchEffect.js
@@ -1,29 +1,70 @@
+var glitchIntervalArray = [];
+
 function disableGlitchEffect(){
+  clearIntervalAnimations();
    //Object Array
    var glitchObjectArray = getGlitchObjects();
 
    //Disable all Objects
    for(var i = 0; i < glitchObjectArray.length; i++){
-     glitchObjectArray[i].className += " disableGlitch";
+     glitchObjectArray[i][0].className += " disableGlitch";
    }
 }
 
 function enableGlitchEffect(){
+   startIntvervalAnimations();
   //Object Array
   var glitchObjectArray = getGlitchObjects();
 
   for(var i = 0; i < glitchObjectArray.length; i++){
-    var currentElementClassName = glitchObjectArray[i].className;
+    if(!glitchObjectArray[i][1]){
+    var currentElementClassName = glitchObjectArray[i][0].className;
 
-    glitchObjectArray[i].className = currentElementClassName.replace("disableGlitch", "");
+    glitchObjectArray[i][0].className = currentElementClassName.replace("disableGlitch", "");
   }
+ }
+}
+
+function clearIntervalAnimations(){
+
+  for(var i = 0; i < glitchIntervalArray.length; i++){
+    clearInterval(glitchIntervalArray[i]);
+  }
+
+}
+
+function setAnimationInterval(elementID, intervalTime, turnOffTimeout){
+  glitchIntervalArray.push(
+    setInterval(function() {
+       var animationObject = document.getElementById(elementID);
+
+       if(animationObject.className.includes("disableGlitch")){
+         animationObject.className = animationObject.className.replace("disableGlitch", "");
+       }
+
+         setTimeout(function() {
+
+          if(!animationObject.className.includes("disableGlitch")){
+             animationObject.className = (animationObject.className + " disableGlitch").trim();
+          }
+
+         }, turnOffTimeout);
+
+    }, intervalTime)
+  );
 }
 
 
-function getGlitchObjects(){
-  var glitchObjectArray = [];
+function startIntvervalAnimations(){
+    setAnimationInterval("cancelText", 4000, 200);
+}
 
-  glitchObjectArray.push(document.getElementById("gameLogin")); //Main Login
+function getGlitchObjects(){
+  var glitchObjectArray = [];//[element, hasInterval]
+
+  glitchObjectArray.push([document.getElementById("gameLogin"), false]); //Main Login
+  glitchObjectArray.push([document.getElementById("cancelButtonGlitchBox"), false]);//Glitch around cancel Button box
+  glitchObjectArray.push([document.getElementById("cancelText"), true]);//cancelButton text glitch
 
   return glitchObjectArray
 }
