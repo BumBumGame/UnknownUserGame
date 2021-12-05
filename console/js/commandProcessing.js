afde3872b891af0df2304fec36435e0c85d3ffc5
Tim <tim555@t-online.de>
2021-12-05 20:26:29 +0100

    Updated standard console Functions - helpDialog now supports variable commandDefinitions - added return to exit function - Standard commands are now automatically added to new programs

--- console/js/commandProcessing.js
+++ console/js/commandProcessing.js
@@ -128,6 +128,14 @@ addProgram(programStartAlias, programDescription, programExecutionReference, exi
   if(exitable){
     programExecutionReference.addCommand("exit", "Exists the current Program", exitProgram);
   }
+  //Add Standard commands
+  //help
+  programExecutionReference.addCommand("help", "Zeigt eine Liste der Verfügbaren Commands mit ihrer Beschreibung an. \n"
+                      + "Um genauere Informationen über einen Befehl zu erhalten schreiben sie: help [Befehl].", showHelpDialog);
+
+  //clear
+  programExecutionReference.addCommand("clear", "Löscht den Kompletten Kommando-Log.", executeClearConsole);
+
   //Create new program
   var newProgram = new Command(programStartAlias, programDescription, true, programExecutionReference);
   //add program to list
@@ -328,29 +336,32 @@ return commandResponse;
 function exitProgram(command, executingConsole){
   //Call close function of executing Console
   executingConsole.closeProgram();
+
+  return null;
 }
 
 /**
 * Function that displays help Dialog
 * @param {String} command command which has been put in the console
+* @param {InGameConsole} executingConsole console Object which the command has been executed on
 **/
-function showHelpDialog(command){
+function showHelpDialog(command, executingConsole){
   //Split Command into Parameters
-  var commandParameters = command.split(" ");
+  let commandParameters = command.split(" ");
   //Define Output array
-  var outputArray;
+  let outputArray = [];
 
   //Print Command list if no Argument is given
   if(commandParameters.length <= 1){
   //define normal Start output
-   outputArray = localCommands.getCommandDescription(localCommands.getCommandIndex("help"));
+   outputArray = executingConsole.currentActiveCommandDefinition.getCommandDescription(executingConsole.currentActiveCommandDefinition.getCommandIndex("help"));
    outputArray.push("");//Empty line
 
    //Print each first line
    //First print local commands
-    for(var i = 0; i < localCommands.localCommandCount; i++){
-      outputArray.push(localCommands.getCommandAlias(i) + ": ");
-      outputArray.push("    " +localCommands.getFirstLineOfCommandDescription(i));
+    for(var i = 0; i < executingConsole.currentActiveCommandDefinition.length; i++){
+      outputArray.push(executingConsole.currentActiveCommandDefinition.getCommandAlias(i) + ": ");
+      outputArray.push("    " + executingConsole.currentActiveCommandDefinition.getFirstLineOfCommandDescription(i));
       //Push Empty line
       outputArray.push("");
     }
@@ -360,11 +371,11 @@ function showHelpDialog(command){
   }else{
     //Take second Command and try to identify command and show Full description
     //First Check local Commands
-    var secondCommandIndex = localCommands.getCommandIndex(commandParameters[1]);
+    var secondCommandIndex = executingConsole.currentActiveCommandDefinition.getCommandIndex(commandParameters[1]);
 
     if(secondCommandIndex != -1){
        //Second command found locally
-       outputArray = localCommands.getCommandDescription(secondCommandIndex);
+       outputArray = executingConsole.currentActiveCommandDefinition.getCommandDescription(secondCommandIndex);
     }else{
       //Check Server Commands
       outputArray = ["Befehl '"+ commandParameters[1] +"' nicht gefunden!"];
