d8ddefb11fd350ba2a52e58b1a941cd36f5fbe18
Tim <tim555@t-online.de>
2021-12-06 12:29:58 +0100

    Moved console create Code from basicTerminal in newGame.js - Moved create console code - removed file basicTerminal.js

--- console/js/newGame.js
+++ console/js/newGame.js
@@ -1,3 +1,17 @@
+//create console
+
+//Console log object
+const consoleLog = document.getElementById("consoleLog");
+//Console input Object
+const consoleInput = document.getElementById("mainConsoleInput");
+//CommandLine Object
+const commandLine = document.getElementById("commandLine");
+
+//Attach functionality Object to console-----------------------------------------
+const mainConsoleObject = new InGameConsole(consoleLog, consoleInput, commandLine, localCommands);
+//-------------------------------------------------------------------------------
+
+
 function startNewGame(){
    mainConsoleObject.disableCommandInput()
    var loadingQue = consoleLoadingAnimation();
