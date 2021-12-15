6eb14d06e8237db5c98a52e8c7bea4a45301b37b
Tim <tim555@t-online.de>
2021-12-15 17:46:03 +0100

    Added Module Jsdoc to module

--- console/js/modules/communicatorProgram.js
+++ console/js/modules/communicatorProgram.js
@@ -1,3 +1,9 @@
+/**
+* Module for the communicator functions
+* (can be set to offline mode with an xml file)
+* @module
+**/
+
 //Communicator functions-------
 function checkForNewMassages(command, executingConsole){
 
