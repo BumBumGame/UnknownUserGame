fb855d11c41200aa62aa91c4978e898f964a2abf
Tim <tim555@t-online.de>
2021-12-31 00:01:22 +0100

    Added subfunctions to communicatorProgram.js Module - tested all new fully implemented Methods

--- console/js/modules/communicatorProgram.js
+++ console/js/modules/communicatorProgram.js
@@ -3,19 +3,75 @@
 * @module Communicator
 **/
 
+//Load asnyLoaderModule
+import * as asyncLoader from "./asyncLoader.js";
+
 //Global Module variable for xmlFile (null if on online mode)
 var xmlFile = null;
 
-//Communicator functions-------
-export function checkForNewMassages(command, executingConsole){
-//implement function
-}
-//----------------------------
+//Global Variable for saving OfflineXML Conditions
+var offlineXMLConditions = [];
+
+//Global Variables which hold the current xmlParser Position
+var currentParserBranchPosition = []; //Array that hold the indexes of each next branch
+var currentParserMessagePosition = 0; //Index of the current Message inside the branch
 
 /**
-* Method which sets the Communicator into offline mode and uses the xml File given as a parameter
+* This Function request the latest Message in a specific Chat or the offlineXml Chat
+* @async
+* @param {String} chatName The Name of the chat (a.e filename) - If an offlineXml is loaded this Parameter does not have any effect
+* @return {String} Next newest unread message or the last read one if no new ones are available
+**/
+export async function getLatestMessage(chatName){
+
+  //Check if offline xml is loaded
+  if(isInOfflineMode()){
+    //load latest Message from offline XML
+
+  }else{
+    //request latest message from Server for chatName
+  }
+
+}
+
+/**
+* Returns Message Content at specific position
+**/
+function getOfflineXMLMessageContent(branchIndex, messageIndex){
+
+}
+
+/**
+* Downloads the xml on the path and sets the module into offline mode with downloaded xml
+* @async
 * @param {String} pathToXML Path to the xml File to be used
 **/
-export function setOfflineXML(pathToXML){
+export async function setOfflineXML(pathToXML){
+  xmlFile = await asyncLoader.loadFileFromServerAsXml(pathToXML);
+}
 
+/**
+* Function that returns the offlineXml File or null if Communicator is in online Mode
+* @return {XMLDocument|null} Current active offlineXml or null if in online Mode
+**/
+export function getOfflineXML(){
+  return xmlFile;
+}
+
+/**
+* Sets communicator back into online Mode. Clears all offlineXML Data!
+**/
+export function setToOnlineMode(){
+  xmlFile = null;
+  var offlineXMLConditions = [];
+  currentParserBranchPosition = [];
+  currentParserMessagePosition = 0;
+}
+
+/**
+* Checks if Communicator is in offline Mode or not
+* @return {boolean} Communicator is in OfflineMode or not
+**/
+export function isInOfflineMode(){
+   return xmlFile == null ? false : true;
 }
